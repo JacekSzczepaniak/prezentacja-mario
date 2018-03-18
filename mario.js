79f7edbf04d192c8d517c176aa90a3b0f2315559
jacek.szczepaniak <jacek.szczepaniak@softfin.pl>
2018-03-18 09:52:09 +0100

    wersja 2

---
+++ mario.js
@@ -0,0 +1,15 @@
+console.log('start');
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'container', { preload: preload, create: create });
+console.log(game);
+
+	function preload() {
+			}
+
+	
+			function create() {
+			}
+
+			function update() {
+
+			}
